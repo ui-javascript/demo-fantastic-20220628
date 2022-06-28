80e6900f820689d11acc2fbbd1762a889ee5cdf4
1095847440@qq.com <1095847440@qq.com>
2022-06-28 18:06:06 +0800

    adjust

---
+++ README.md
@@ -0,0 +1,7 @@
+# README
+
+fantastic-admin 目录结构学习
+
+---
+
+# 参考 @ref
---
+++ demo/src/views/index2.vue
@@ -0,0 +1,13 @@
+<route>
+{
+    meta: {
+        layout: false
+    }
+}
+</route>
+
+<template>
+    <div>
+        欢迎使用 Fantastic-startkit ！2222
+    </div>
+</template>
